75302d31fe9b95ea2647abc3d94a2236577741e5
huxiaowei <525782303@qq.com>
2021-07-15 11:09:05 +0800

    流

--- src/main/java/lambdasinaction/chap05/Filtering.java
+++ src/main/java/lambdasinaction/chap05/Filtering.java
@@ -8,10 +8,13 @@ import java.util.List;
 
 import lambdasinaction.chap04.Dish;
 
+/**
+ *
+ */
 public class Filtering {
 
   public static void main(String... args) {
-    // Filtering with predicate
+    // Filtering with predicate 筛选操作
     System.out.println("Filtering with a predicate");
     List<Dish> vegetarianMenu = menu.stream()
         .filter(Dish::isVegetarian)
@@ -26,30 +29,58 @@ public class Filtering {
         .distinct()
         .forEach(System.out::println);
 
-    // Slicing a stream
+    // Slicing a stream  分片流
     // This list is sorted in ascending order of number of calories!
     List<Dish> specialMenu = Arrays.asList(
         new Dish("season fruit", true, 120, Dish.Type.OTHER),
-        new Dish("prawns", false, 300, Dish.Type.FISH),
         new Dish("rice", true, 350, Dish.Type.OTHER),
+        new Dish("prawns", false, 300, Dish.Type.FISH),
         new Dish("chicken", false, 400, Dish.Type.MEAT),
         new Dish("french fries", true, 530, Dish.Type.OTHER));
-    System.out.println("Filtered sorted menu:");
-    List<Dish> filteredMenu = specialMenu.stream()
-        .filter(dish -> dish.getCalories() < 320)
-        .collect(toList());
+    System.out.println("使用Filter对菜单进行筛选:"); //找出小于320卡热量的菜单
+    List<Dish> filteredMenu = specialMenu.stream().filter(dish ->
+    {
+      System.out.println(dish.getName());
+      return dish.getCalories() < 320;
+    }).collect(toList());
+    /*filter遍历的打印结果:
+      season fruit
+      prawns
+      rice
+      chicken
+      french frie
+    */
+    System.out.println("filter筛选结果:");
     filteredMenu.forEach(System.out::println);
 
-    System.out.println("Sorted menu sliced with takeWhile():");
+    System.out.println("使用takeWhile()对菜单进行筛选:"); //找出小于320卡热量的菜单
     List<Dish> slicedMenu1 = specialMenu.stream()
-        .takeWhile(dish -> dish.getCalories() < 320)
+        .takeWhile(dish -> {
+          System.out.println(dish.getName());
+          return dish.getCalories() < 320; ////一旦遇到元素的断言为false就停止处理
+        })
         .collect(toList());
+    /*takeWhile方法遍历打印的结果:
+      season fruit
+      rice
+      重点: 你可以看到，takeWhile一旦遇到第一个大于320卡路里的rice时，就停止了遍历，而前面的filter是遍历了所有的元素
+    */
+    System.out.println("takeWhile的筛选结果:");
     slicedMenu1.forEach(System.out::println);
 
-    System.out.println("Sorted menu sliced with dropWhile():");
+    System.out.println("使用dropWhile()对菜单进行筛选:"); //找出大于320卡热量的菜单
     List<Dish> slicedMenu2 = specialMenu.stream()
-        .dropWhile(dish -> dish.getCalories() < 320)
-        .collect(toList());
+        .dropWhile(dish -> {
+          System.out.println(dish.getName());
+          return dish.getCalories() < 320;  //一旦遇到元素的断言为false也和takewhile一样就停止处理，但不同的是，dropwhile会丢弃该元素，并返回所有剩余的元素
+        }).collect(toList());
+    /*dropWhile方法遍历打印的结果:
+      season fruit
+      rice
+
+
+     */
+    System.out.println("dropWhile的筛选结果:");
     slicedMenu2.forEach(System.out::println);
 
     // Truncating a stream
---
+++ src/main/java/lambdasinaction/chap05/Test.java
@@ -0,0 +1,36 @@
+package lambdasinaction.chap05;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @version 1.0
+ * @Description: dropWhile
+ * @author: bingyu
+ * @date: 2021/7/15
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        List<String> list = Stream.of("a", "b", "c", "", "e", "f").dropWhile(s -> {
+            System.out.println(s);
+            return !s.isEmpty();  //s为a时，当前返回的就是true，后面继续进行了遍历
+        }).collect(toList());
+        System.out.println("---------------------");
+        list.forEach(System.out::println);
+
+        /*输出结果:
+        a
+        b
+        c
+
+        ---------------------
+
+        e
+        f
+
+         */
+    }
+}
