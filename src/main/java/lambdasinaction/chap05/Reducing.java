0b89c45e9123f6dfbb190ec7725ec72575ec003c
huxiaowei <525782303@qq.com>
2021-07-18 22:33:36 +0800

    归约操作

--- src/main/java/lambdasinaction/chap05/Reducing.java
+++ src/main/java/lambdasinaction/chap05/Reducing.java
@@ -6,16 +6,23 @@ import java.util.Optional;
 
 import lambdasinaction.chap04.Dish;
 
+/**
+ * 归约操作
+ */
 public class Reducing {
 
   public static void main(String... args) {
+    //1. 元素求和
     List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
-    int sum = numbers.stream().reduce(0, (a, b) -> a + b);
+    //reduce方法 参数1:累积函数的初始值  参数2:用于将2个值组合成一个值的lambda表达式
+    int sum = numbers.stream().reduce(0, (a, b) -> a + b); //这里就是0+3=3 、3+4=7........
     System.out.println(sum);
 
+    //2. 元素求和，使用了"类::静态方法"引用
     int sum2 = numbers.stream().reduce(0, Integer::sum);
     System.out.println(sum2);
 
+    //3.获取最大值
     int max = numbers.stream().reduce(0, (a, b) -> Integer.max(a, b));
     System.out.println(max);
 
