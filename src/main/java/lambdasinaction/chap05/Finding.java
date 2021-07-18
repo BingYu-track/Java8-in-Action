50424b691178fac9f64c19ed2133f2adecffa6e5
huxiaowei <525782303@qq.com>
2021-07-18 21:27:23 +0800

    查找和匹配

--- src/main/java/lambdasinaction/chap05/Finding.java
+++ src/main/java/lambdasinaction/chap05/Finding.java
@@ -1,21 +1,39 @@
 package lambdasinaction.chap05;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import lambdasinaction.chap04.Dish;
 
+/**
+ * 查找和匹配
+ */
 public class Finding {
 
   public static void main(String... args) {
+    //anyMatch
     if (isVegetarianFriendlyMenu()) {
       System.out.println("Vegetarian friendly");
     }
 
+    //allMatch
     System.out.println(isHealthyMenu());
+    //noneMatch
     System.out.println(isHealthyMenu2());
 
+    //findAny
     Optional<Dish> dish = findVegetarianDish();
     dish.ifPresent(d -> System.out.println(d.getName()));
+
+    //findFirst
+    //找出第一个平方能被3整除的数
+    List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5);
+    Optional<Integer> firstSquareDivisibleByThree =
+            someNumbers.stream()
+                    .map(n -> n * n)
+                    .filter(n -> n % 3 == 0)
+                    .findFirst(); // 9
   }
 
   private static boolean isVegetarianFriendlyMenu() {
