ce6f2575f579b4bc38f013788eb89f278902bc5b
huxiaowei <525782303@qq.com>
2021-07-21 08:45:18 +0800

    构建流

--- src/main/java/lambdasinaction/chap05/BuildingStreams.java
+++ src/main/java/lambdasinaction/chap05/BuildingStreams.java
@@ -1,5 +1,6 @@
 package lambdasinaction.chap05;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,21 +9,50 @@ import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+/**
+ * 构建流
+ */
 public class BuildingStreams {
 
   public static void main(String... args) throws Exception {
-    // Stream.of
+    //1.通过Stream.of用数值来创建流
     Stream<String> stream = Stream.of("Java 8", "Lambdas", "In", "Action");
     stream.map(String::toUpperCase).forEach(System.out::println);
 
-    // Stream.empty
+    // Stream.empty调用empty方法创建空的流
     Stream<String> emptyStream = Stream.empty();
 
-    // Arrays.stream
+    //2.空对象创建流
+    String homeValue = System.getProperty("home");
+    Stream<String> homeValueStream = homeValue == null ? Stream.empty() : Stream.of(homeValue);
+
+    //借助于java9中的Stream.ofNullable方法，这段代码可以改写得更加简洁：
+    Stream<String> homeValueStream2 = Stream.ofNullable(System.getProperty("home"));
+
+
+    //3.Arrays.stream数组创建流
     int[] numbers = { 2, 3, 5, 7, 11, 13 };
     System.out.println(Arrays.stream(numbers).sum());
 
-    // Stream.iterate
+    //4. Files.lines文件生成流
+    long uniqueWords = 0;
+    //流会自动关闭，因此不需要执行额外的try-finally 操作
+    try(Stream<String> lines = Files.lines(Paths.get("lambdasinaction/chap5/data.txt"), Charset.defaultCharset())){
+      uniqueWords = lines.flatMap(line -> Arrays.stream(line.split(" ")))//生成单词流
+                        .distinct()
+                        .count();
+      System.out.println("There are " + uniqueWords + " unique words in data.txt");
+    }
+    catch(IOException e){ //如果打开文件时出现异常则加以处理
+
+    }
+
+
+    /**
+     * 5.函数生成流
+     */
+
+    // 5.1 Stream.iterate
     Stream.iterate(0, n -> n + 2)
         .limit(10)
         .forEach(System.out::println);
@@ -72,12 +102,7 @@ public class BuildingStreams {
         .limit(10)
         .forEach(System.out::println);
 
-    long uniqueWords = Files.lines(Paths.get("lambdasinaction/chap5/data.txt"), Charset.defaultCharset())
-        .flatMap(line -> Arrays.stream(line.split(" ")))
-        .distinct()
-        .count();
 
-    System.out.println("There are " + uniqueWords + " unique words in data.txt");
   }
 
 }
