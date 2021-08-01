82c94b06260a09716e3e7f1c67309ff8dcfafd9a
huxiaowei <525782303@qq.com>
2021-08-01 23:48:37 +0800

    1111

--- src/main/java/lambdasinaction/chap07/WordCount.java
+++ src/main/java/lambdasinaction/chap07/WordCount.java
@@ -2,31 +2,40 @@ package lambdasinaction.chap07;
 
 import java.util.Spliterator;
 import java.util.function.Consumer;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+/**
+ *
+ */
 public class WordCount {
 
+  //句子里添加了一些额外的随机空格，以演示这个迭代实现即使在两个词之间存在多个空格时也能正常工作
   public static final String SENTENCE =
       " Nel   mezzo del cammin  di nostra  vita "
       + "mi  ritrovai in una  selva oscura"
       + " che la  dritta via era   smarrita ";
 
   public static void main(String[] args) {
-    System.out.println("Found " + countWordsIteratively(SENTENCE) + " words");
+    //System.out.println("Found " + countWordsIteratively(SENTENCE) + " words");
     System.out.println("Found " + countWords(SENTENCE) + " words");
   }
 
+  /**
+   * 单词计数器：判断给定的字符串有多少个单词(根据空格区分的)
+   * @param s 字符串
+   * @return
+   */
   public static int countWordsIteratively(String s) {
-    int counter = 0;
+    int counter = 0; //单词计数器
     boolean lastSpace = true;
-    for (char c : s.toCharArray()) {
+    for (char c : s.toCharArray()) { //逐个遍历字符串中的所有字符
       if (Character.isWhitespace(c)) {
         lastSpace = true;
-      }
-      else {
+      } else {
         if (lastSpace) {
-          counter++;
+          counter++; //上一个字符是空格，而当前遍历的字符不是空格时，将单词计数器加一
         }
         lastSpace = Character.isWhitespace(c);
       }
@@ -34,21 +43,33 @@ public class WordCount {
     return counter;
   }
 
+  /**
+   * 函数式风格重写单词计数器
+   * @param s
+   * @return
+   */
   public static int countWords(String s) {
-    //Stream<Character> stream = IntStream.range(0, s.length())
-    //    .mapToObj(SENTENCE::charAt).parallel();
-    Spliterator<Character> spliterator = new WordCounterSpliterator(s);
-    Stream<Character> stream = StreamSupport.stream(spliterator, true);
+    System.out.println(s.length()); //109个字符
+    Stream<Character> stream = IntStream.range(0, s.length()) //遍历从0开始到字符串的长度
+            .mapToObj(SENTENCE::charAt) //将数字映射成字符
+            .parallel();
+//    Spliterator<Character> spliterator = new WordCounterSpliterator(s);
+//    Stream<Character> stream = StreamSupport.stream(spliterator, true);
 
     return countWords(stream);
   }
 
+  /**
+   * 统计
+   * @param stream
+   * @return
+   */
   private static int countWords(Stream<Character> stream) {
     WordCounter wordCounter = stream.reduce(new WordCounter(0, true), WordCounter::accumulate, WordCounter::combine);
     return wordCounter.getCounter();
   }
 
-  private static class WordCounter {
+  private static class WordCounter { //这是一个Immutable class，因为我们无法改变这个类的对象的状态
 
     private final int counter;
     private final boolean lastSpace;
@@ -58,15 +79,22 @@ public class WordCount {
       this.lastSpace = lastSpace;
     }
 
+    //和迭代算法一样，accumulate方法一个个遍历Character
     public WordCounter accumulate(Character c) {
-      if (Character.isWhitespace(c)) {
-        return lastSpace ? this : new WordCounter(counter, true);
+      if (Character.isWhitespace(c)) { //判断当前字符是否是空格
+        return lastSpace ?
+                this :  //上一个字符是空格，当前遍历的字符也是空格
+                new WordCounter(counter, true); //上一个字符不是空格，当前遍历的字符是空格
+      } else {
+        //执行到这里，说明当前字符不是空格
+        return lastSpace ?
+                new WordCounter(counter + 1, false) :  //上一个字符是空格，而当前遍历的字符不是空格时，将单词计数器加一
+                this; //上一个字符不是空格，当前遍历的字符也不是空格
       }
-      else {
-        return lastSpace ? new WordCounter(counter + 1, false) : this;
-      }
+
     }
 
+    //合并两个WordCounter，把其计数器加起来
     public WordCounter combine(WordCounter wordCounter) {
       return new WordCounter(counter + wordCounter.counter, wordCounter.lastSpace);
     }
@@ -77,6 +105,9 @@ public class WordCount {
 
   }
 
+  /**
+   * 基于单词计数器的可分迭代器
+   */
   private static class WordCounterSpliterator implements Spliterator<Character> {
 
     private final String string;
