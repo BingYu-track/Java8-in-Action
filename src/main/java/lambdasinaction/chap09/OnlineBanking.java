aa77f80a50a55e936e5937e6aff22e4e76d0b44d
huxiaowei <525782303@qq.com>
2021-08-04 17:12:48 +0800

    lambda实现模板方法设计模式

--- src/main/java/lambdasinaction/chap09/OnlineBanking.java
+++ src/main/java/lambdasinaction/chap09/OnlineBanking.java
@@ -1,20 +1,25 @@
 package lambdasinaction.chap09;
 
+/**
+ * 传统的模板方法设计模式
+ * 模板方法（Template Method）模式的定义如下：定义一个操作中的算法骨架，而将算法的一些步骤延迟到子类中，使得子类可以不改变该算法结构的情况下
+ * 重定义该算法的某些特定步骤。它是一种类行为型模式。
+ */
 abstract class OnlineBanking {
 
   public void processCustomer(int id) {
     Customer c = Database.getCustomerWithId(id);
-    makeCustomerHappy(c);
+    makeCustomerHappy(c); //使客户满意
   }
 
-  abstract void makeCustomerHappy(Customer c);
+  abstract void makeCustomerHappy(Customer c); //用来给子类重新实现
 
-  // dummy Customer class
+  // dummy Customer class 用户信息
   static private class Customer {}
 
-  // dummy Database class
+  // dummy Database class 数据库
   static private class Database {
-
+    //根据用户id得到用户信息
     static Customer getCustomerWithId(int id) {
       return new Customer();
     }
--- src/main/java/lambdasinaction/chap09/OnlineBankingLambda.java
+++ src/main/java/lambdasinaction/chap09/OnlineBankingLambda.java
@@ -2,10 +2,16 @@ package lambdasinaction.chap09;
 
 import java.util.function.Consumer;
 
+/**
+ * lambda实现模板方法设计模式
+ */
 public class OnlineBankingLambda {
 
   public static void main(String[] args) {
+    //通过传递Lambda 表达式，直接插入不同的行为，不再需要再继承OnlineBanking类了
+    //佐证了Lambda表达式能帮助你解决设计模式与生俱来的设计僵化问题
     new OnlineBankingLambda().processCustomer(1337, (Customer c) -> System.out.println("Hello!"));
+    new OnlineBankingLambda().processCustomer(1337, (Customer c) -> System.out.println("Hello " + c.getName()));
   }
 
   public void processCustomer(int id, Consumer<Customer> makeCustomerHappy) {
@@ -14,7 +20,17 @@ public class OnlineBankingLambda {
   }
 
   // dummy Customer class
-  static private class Customer {}
+  static private class Customer {
+    private String name;
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
 
   // dummy Database class
   static private class Database {
