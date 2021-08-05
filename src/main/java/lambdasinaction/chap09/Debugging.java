de762058d5e7a61acc08957976206f4536ef725e
huxiaowei <525782303@qq.com>
2021-08-05 23:10:22 +0800

    lambda表达式的2种调试方法

--- src/main/java/lambdasinaction/chap09/Debugging.java
+++ src/main/java/lambdasinaction/chap09/Debugging.java
@@ -3,11 +3,37 @@ package lambdasinaction.chap09;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 查看栈跟踪帮助调试lambda表达式的
+ */
 public class Debugging {
+
+  public static int divideByZero(int n){
+    return n / 0;
+  }
 
   public static void main(String[] args) {
     List<Point> points = Arrays.asList(new Point(12, 2), null);
     points.stream().map(p -> p.getX()).forEach(System.out::println);
+    /*
+      异常信息:
+       Exception in thread "main" java.lang.NullPointerException
+	      at lambdasinaction.chap09.Debugging.lambda$main$0(Debugging.java:17)
+          .............
+
+        因为Lambda 表达式没有名字，所以编译器只能为它们指定一个名字。在这个例子中，它的名字是lambda$main$0，看起来非常不直观
+     */
+
+    //但是，如果方法引用指向的是同一个类中声明的方法，那么它的名称是可以在栈跟踪中显示的。
+    List<Integer> numbers = Arrays.asList(1, 2, 3);
+    numbers.stream().map(Debugging::divideByZero).forEach(System
+            .out::println);
+    /*
+        异常信息:
+        Exception in thread "main" java.lang.ArithmeticException: / by zero
+	      at lambdasinaction.chap09.Debugging.divideByZero(Debugging.java:12)  正确的看到了抛异常的方法信息
+	      .................
+     */
   }
 
   private static class Point {
--- src/main/java/lambdasinaction/chap09/Peek.java
+++ src/main/java/lambdasinaction/chap09/Peek.java
@@ -5,18 +5,39 @@ import static java.util.stream.Collectors.toList;
 import java.util.List;
 import java.util.stream.Stream;
 
+/**
+ * 使用日志调试Lambda----使用peek查看Stream流中的数据流的值
+ */
 public class Peek {
+
 
   public static void main(String[] args) {
     List<Integer> result = Stream.of(2, 3, 4, 5)
         .peek(x -> System.out.println("taking from stream: " + x))
         .map(x -> x + 17)
-        .peek(x -> System.out.println("after map: " + x))
+        .peek(x -> System.out.println("after map: " + x)) //输出map操作的结果
         .filter(x -> x % 2 == 0)
-        .peek(x -> System.out.println("after filter: " + x))
+        .peek(x -> System.out.println("after filter: " + x)) //输出经过filter操作之后，剩下的元素
         .limit(3)
         .peek(x -> System.out.println("after limit: " + x))
         .collect(toList());
+
+    /*
+      通过peek操作能清楚地了解流水线操作中每一步的输出结果
+      输出:
+      taking from stream: 2
+      after map: 19
+      taking from stream: 3
+      after map: 20
+      after filter: 20
+      after limit: 20
+      taking from stream: 4
+      after map: 21
+      taking from stream: 5
+      after map: 22
+      after filter: 22
+      after limit: 22
+     */
   }
 
 }
