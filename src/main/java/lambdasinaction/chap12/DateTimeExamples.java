0796c9f2cfcbd8db02db680088c1e0825770da4e
huxiaowei <525782303@qq.com>
2021-09-22 22:21:09 +0800

    Duration和Period

--- src/main/java/lambdasinaction/chap12/DateTimeExamples.java
+++ src/main/java/lambdasinaction/chap12/DateTimeExamples.java
@@ -5,14 +5,9 @@ import static java.time.temporal.TemporalAdjusters.nextOrSame;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.DayOfWeek;
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
+import java.time.*;
 import java.time.chrono.JapaneseDate;
+import java.time.chrono.MinguoDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
@@ -51,50 +46,95 @@ public class DateTimeExamples {
     System.out.println(calendar);
   }
 
+
   private static void useLocalDate() {
+    /**
+     * 1.使用LocalDate，LocalDate只包含日期，不包含当天的时间信息
+     */
     LocalDate date = LocalDate.of(2014, 3, 18);
     int year = date.getYear(); // 2014
     Month month = date.getMonth(); // MARCH
     int day = date.getDayOfMonth(); // 18
     DayOfWeek dow = date.getDayOfWeek(); // TUESDAY
     int len = date.lengthOfMonth(); // 31 (days in March)
-    boolean leap = date.isLeapYear(); // false (not a leap year)
+    boolean leap = date.isLeapYear(); // false (not a leap year)，判断是否是闰年
     System.out.println(date);
 
+    //使用工厂方法now从系统时钟中获取当前的日期:
+    LocalDate today = LocalDate.now(); //2021-09-17
+
+    //TemporalField的实现类ChronoField，用来读取LocalDate的值
     int y = date.get(ChronoField.YEAR);
     int m = date.get(ChronoField.MONTH_OF_YEAR);
     int d = date.get(ChronoField.DAY_OF_MONTH);
 
+    /**
+     * 2. LocalTime只用来表示一天中的时间
+     */
     LocalTime time = LocalTime.of(13, 45, 20); // 13:45:20
     int hour = time.getHour(); // 13
     int minute = time.getMinute(); // 45
     int second = time.getSecond(); // 20
     System.out.println(time);
 
+    //LocalDate 和LocalTime 都可以通过解析代表它们的字符串创建。使用静态方法parse，
+    LocalDate ld = LocalDate.parse("2017-09-21");
+    LocalTime lt = LocalTime.parse("13:45:20");
+
+
+    /**
+     * 3.LocalDateTime，是合并了LocalDate和LocalTime的集合体，它同时表示了日期和时间，但不带时区信息
+     */
+    //直接创建LocalDateTime
     LocalDateTime dt1 = LocalDateTime.of(2014, Month.MARCH, 18, 13, 45, 20); // 2014-03-18T13:45
+    //通过合并LocalDate和LocalTime创建
     LocalDateTime dt2 = LocalDateTime.of(date, time);
-    LocalDateTime dt3 = date.atTime(13, 45, 20);
-    LocalDateTime dt4 = date.atTime(time);
-    LocalDateTime dt5 = time.atDate(date);
+    LocalDateTime dt3 = date.atTime(13, 45, 20); //向LocalDate传递时间合并成LocalDateTime
+    LocalDateTime dt4 = date.atTime(time); //向LocalDate传递LocalTime合并成LocalDateTime
+    LocalDateTime dt5 = time.atDate(date); //向LocalTime传递LocalDate合并成LocalDateTime
     System.out.println(dt1);
 
+//  前面展示了向LocalDate 传递一个时间对象，或者向LocalTime 传递一个日期对象的方式，你可以创建一个LocalDateTime 对象。
+//  下面的示例展示了，你也可以使用toLocalDate 或者toLocalTime 方法，从LocalDateTime 中提取LocalDate或者LocalTime 组件:
     LocalDate date1 = dt1.toLocalDate();
     System.out.println(date1);
     LocalTime time1 = dt1.toLocalTime();
     System.out.println(time1);
 
-    Instant instant = Instant.ofEpochSecond(44 * 365 * 86400);
+    /**
+     * Instant是为了便于机器处理的日期和时间格式
+     */
+    Instant instant = Instant.ofEpochSecond(44 * 365 * 86400); //从1970年1月1日零点开始后的365天*44所对应的时间，是一个unix时间戳
     Instant now = Instant.now();
+    Instant.ofEpochSecond(3);
+    Instant.ofEpochSecond(3, 0);
+    Instant.ofEpochSecond(2, 1_000_000_000); //2 秒之后再加上10亿纳秒（1 秒）
+    Instant.ofEpochSecond(4, -1_000_000_000); //4 秒之前的10亿纳秒（1 秒）
 
-    Duration d1 = Duration.between(LocalTime.of(13, 45, 10), time);
-    Duration d2 = Duration.between(instant, now);
-    System.out.println(d1.getSeconds());
-    System.out.println(d2.getSeconds());
+    /**
+     * 4.Duration和Period
+     */
+    //(1)、Duration是用于以'秒'和'纳秒'来衡量2个时间点的时间长短的，所以不能仅向between方法传递一个LocalDate，必须有time才行
+    Duration d1 = Duration.between(LocalTime.of(13, 45, 10), time); //13:45:10和13:45:20之间的时长
+    Duration d2 = Duration.between(instant, now); //instant和现在的时间时长
+    System.out.println(d1.getSeconds()); //相隔10秒
+    System.out.println(d2.getSeconds()); //相隔244734651秒
 
-    Duration threeMinutes = Duration.of(3, ChronoUnit.MINUTES);
+    //(2)、如果需要以年、月、日的方式衡量，可以使用Period，使用该类的between方法，你可以得到两个LocalDate之间的时长
+    Period tenDays = Period.between(LocalDate.of(2017, 9, 11),
+                                    LocalDate.of(2017, 9, 21));
+
+
+    Duration threeMinutes = Duration.of(3, ChronoUnit.MINUTES); //ChronoUnit是表示Period的时间单位，这里表示是3分钟
     System.out.println(threeMinutes);
 
-    JapaneseDate japaneseDate = JapaneseDate.from(date);
+    threeMinutes = Duration.ofMinutes(3); //3分钟
+    tenDays = Period.ofDays(10);  //10天
+    Period threeWeeks = Period.ofWeeks(3); //3周
+    Period twoYearsSixMonthsOneDay = Period.of(2, 6, 1);
+
+    JapaneseDate japaneseDate = JapaneseDate.from(date); //日本日历系统
+    MinguoDate minguoDate = MinguoDate.from(date); //中华民国日历系统
     System.out.println(japaneseDate);
   }
 
